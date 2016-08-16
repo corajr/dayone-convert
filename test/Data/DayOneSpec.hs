3582f21dd95f1bcfd80eafc976c742a244cdf5da
Cora Johnson-Roberson <me@corajr.com>
2016-08-16 10:01:02 -0400

    check that JSON output meets expectation

--- test/Data/DayOneSpec.hs
+++ test/Data/DayOneSpec.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Data.DayOneSpec (main, spec) where
 
 import Test.Hspec
 
 import Data.String.Here
-import Data.Aeson (decode)
+import GHC.Exts
+import Data.Aeson
 import Data.Time
 import qualified Data.ByteString.Lazy.Char8 as BL
 import Data.DayOne
@@ -57,17 +59,36 @@ exampleEntry = Entry
 blankUUID :: Entry -> Entry
 blankUUID entry = entry { uuid = "" }
 
+exampleDayone :: DayOne
+exampleDayone = DayOne
+  { metadata = defaultMetadata
+  , entries = [exampleEntry]
+  }
+
+exampleEntryObject :: Value
+exampleEntryObject =
+  Object $ fromList [ ("text",String "This is a test.\n")
+                    , ("uuid",String "")
+                    , ("starred",Bool False)
+                    , ("creationDate",String "2016-08-15T22:15:31Z")]
+
+exampleDayoneObject :: Value
+exampleDayoneObject =
+  Object $ fromList [ ("entries", Array (fromList [exampleEntryObject]))
+                    , ("metadata", Object (fromList [("version",String "1.0")]))]
+
 spec :: Spec
 spec = do
   describe "FromJSON instance" $ do
-    it "parses a valid example" $ do
+    it "parses a valid DayOne JSON example" $ do
       decode (BL.pack exampleJson) `shouldBe` Just exampleParsed
+  describe "ToJSON instance" $ do
+    it "outputs valid JSON for a DayOne value" $ do
+      toJSON exampleDayone `shouldBe` exampleDayoneObject
   describe "fromFile" $ do
     it "takes a path to a text file and returns an `Entry`" $ do
       fmap blankUUID (fromFile "test/example/example.txt") `shouldReturn` exampleEntry
   describe "fromDirectory" $ do
     it "takes a directory path and converts all files into a DayOne value" $ do
       let f x = x { entries = map blankUUID (entries x) }
-      fmap f (fromDirectory "test/example") `shouldReturn` DayOne { metadata = defaultMetadata
-                                                                  , entries = [exampleEntry]
-                                                                  }
+      fmap f (fromDirectory "test/example") `shouldReturn` exampleDayone
